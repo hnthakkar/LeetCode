68ce64196d5289d1438195bc718e046853216b7c
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-03 16:26:15 +0530

    Problems of Array Rotate Matrix

---
+++ src/ranktransform/RankTransformTest.java
@@ -0,0 +1,14 @@
+package ranktransform;
+
+import org.junit.jupiter.api.Test;
+import pascal2.Pascal2;
+
+public class RankTransformTest {
+
+    @Test
+    public void testRankTransform() {
+        RankTransform rankTransform = new RankTransform();
+        rankTransform.arrayRankTransform(new int[] {});
+    }
+
+}
