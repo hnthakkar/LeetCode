e10ec2b9affa99a35dc6db3c84fdfcea31012d24
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-08 19:01:39 +0530

    Problems of Array Rotate Matrix

---
+++ src/majorityelement/MajorityElementTest.java
@@ -0,0 +1,13 @@
+package majorityelement;
+
+import maxprofit.MaxProfit;
+import org.junit.jupiter.api.Test;
+
+public class MajorityElementTest {
+
+    @Test
+    public void testMajorityElement() {
+        MajorityElement majorityElement = new MajorityElement();
+        majorityElement.majorityElement(new int[] {2,2,1,1,1,2,2});
+    }
+}
