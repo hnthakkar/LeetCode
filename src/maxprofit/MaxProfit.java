9b52b6a1a21e3156d77121003d732a287a4370a3
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-08 13:59:14 +0530

    Problems of Array Rotate Matrix

---
+++ src/maxprofit/MaxProfit.java
@@ -0,0 +1,25 @@
+package maxprofit;
+
+public class MaxProfit {
+
+    public int maxProfit(int[] prices) {
+        // 7,1,5,3,6,4
+        int len = prices.length;
+        int currentMax = 0;
+        int maxSoFar = 0;
+        int index = 0;
+
+        for (int i = 1; i < len; i++) {
+            currentMax = Math.max(0, prices[i] - prices[index]);
+            if ( currentMax == 0) {
+                index = i;
+            }
+
+            if (currentMax > maxSoFar) {
+                maxSoFar = currentMax;
+            }
+        }
+
+        return maxSoFar;
+    }
+}
