3e91b2320f95eedaeb356e87757d8a155ff8487c
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-08 20:32:18 +0530

    Problems of Array Rotate Matrix

---
+++ src/rotatearray/RotateArrayTest.java
@@ -0,0 +1,18 @@
+package rotatearray;
+
+import org.junit.jupiter.api.Test;
+
+public class RotateArrayTest {
+
+    @Test
+    public void rotateArray() {
+        RotateArray rotateArray = new RotateArray();
+        rotateArray.rotate(new int[] {1,2,3,4,5,6,7}, 3);
+    }
+
+    @Test
+    public void rotateArrayReverse() {
+        RotateArrayReverse rotateArrayReverse = new RotateArrayReverse();
+        rotateArrayReverse.rotate(new int[] {1,2,3,4,5,6,7}, 3);
+    }
+}
