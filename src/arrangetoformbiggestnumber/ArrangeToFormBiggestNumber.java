163f61b5ed873f4aef0f7c36afe3d9863961ff94
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-13 11:12:24 +0530

    Problems of Array Rotate Matrix

---
+++ src/arrangetoformbiggestnumber/ArrangeToFormBiggestNumber.java
@@ -0,0 +1,36 @@
+package arrangetoformbiggestnumber;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ArrangeToFormBiggestNumber {
+
+    public static void main(String[] args) {
+        int[] input = new int[] {1, 34, 3, 98, 9, 76, 45, 4};
+        getMaxNumber(Arrays.stream(input).boxed().toArray( Integer[]::new ));
+    }
+
+    public static String getMaxNumber(Integer nums[]) {
+        Arrays.sort(nums, new NumberConcatenateComparator());
+
+        String max = "";
+
+        for (Integer num: nums) {
+            max += num;
+        }
+
+        return max;
+    }
+}
+
+class NumberConcatenateComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer num1, Integer num2) {
+        String num1_num2 = "" + num1 + num2;
+        String num2_num1 = "" + num2 + num1;
+
+        return num2_num1.compareTo(num1_num2);
+    }
+}
