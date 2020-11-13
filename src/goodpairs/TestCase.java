93196b0b17968adfe4128629680ae378d77a0581
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-13 18:10:26 +0530

    Problems of Array Rotate Matrix

---
+++ src/goodpairs/TestCase.java
@@ -0,0 +1,13 @@
+package goodpairs;
+
+import hasduplicate.DuplicateElement;
+import org.junit.jupiter.api.Test;
+
+public class TestCase {
+
+    @Test
+    public void goodPairTest() {
+        Solution goodPair = new Solution();
+        goodPair.numIdenticalPairs(new int[] {1,2,3,1,1,3});
+    }
+}
