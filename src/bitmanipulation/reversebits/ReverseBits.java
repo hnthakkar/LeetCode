657b59c836c73ce6707eb69cefe5fc40687cd30b
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-15 19:53:42 +0530

    Problems of Array Rotate Matrix

---
+++ src/bitmanipulation/reversebits/ReverseBits.java
@@ -0,0 +1,44 @@
+package bitmanipulation.reversebits;
+
+public class ReverseBits {
+
+    public static void main(String[] args) {
+        reverseBits(0b11111111111111111111111111111101);
+    }
+
+    public static int reverseBits(int n) {
+        String numberAsString = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+
+        char[] numberCharArray = numberAsString.toCharArray();
+
+        int leftIndex = 0;
+        int rightIndex = numberCharArray.length - 1;
+        char tmp;
+        while (leftIndex < rightIndex) {
+            tmp = numberCharArray[rightIndex];
+            numberCharArray[rightIndex] = numberCharArray[leftIndex];
+            numberCharArray[leftIndex] = tmp;
+            leftIndex++;
+            rightIndex--;
+        }
+
+        StringBuffer reverseString = new StringBuffer();
+        for (char ch: numberCharArray) {
+            reverseString.append(ch);
+        }
+
+        return (int) Long.parseLong(reverseString.toString(), 2);
+        //return Integer.parseInt(reverseString.toString(), 2);
+    }
+
+    public static int reverseBitsUsingShift(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;
+            if (i < 31)
+                result <<= 1;
+        }
+        return result;
+    }
+}
