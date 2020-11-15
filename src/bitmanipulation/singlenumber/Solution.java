6f73f49ab016b0909c6f0343a4f058ea766e7636
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2020-11-15 19:57:31 +0530

    Problems of Array Rotate Matrix

---
+++ src/bitmanipulation/singlenumber/Solution.java
@@ -0,0 +1,13 @@
+package bitmanipulation.singlenumber;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+
+        for (int num: nums) {
+            number ^= num;
+        }
+
+        return number;
+    }
+}
