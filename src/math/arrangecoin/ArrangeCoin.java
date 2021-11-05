2975c0d20cc9631e269479277d2a206337ac2573
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2021-11-05 19:46:21 +0530

    Leetcode problem

--- src/math/arrangecoin/ArrangeCoin.java
+++ src/math/arrangecoin/ArrangeCoin.java
@@ -9,8 +9,22 @@ public class ArrangeCoin {
         ac.arrangeCoins(2147483647);
     }
 
-
     public int arrangeCoins(int n) {
+        if (n < 3) {
+            return 1;
+        }
+
+        long completeRow = 1l;
+        long limit = 2l*n;
+
+        while ((completeRow * (completeRow + 1)) <= limit) {
+            completeRow++;
+        }
+
+        return (int)completeRow - 1;
+    }
+
+    /*public int arrangeCoins(int n) {
         long p = 1l;
         long r = 2l * n;
         while (((p*p) + p) <= r) {
@@ -18,5 +32,5 @@ public class ArrangeCoin {
         }
 
         return (int)(p - 1);
-    }
+    }*/
 }
