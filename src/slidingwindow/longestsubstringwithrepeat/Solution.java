4e99635a7dcb7303104c77f78f8eb9706e1627de
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2021-11-24 13:27:53 +0530

    Leetcode problem

--- src/slidingwindow/longestsubstringwithrepeat/Solution.java
+++ src/slidingwindow/longestsubstringwithrepeat/Solution.java
@@ -1,10 +1,44 @@
 package slidingwindow.longestsubstringwithrepeat;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
 
+    public static void main(String[] str) {
+        Solution sol = new Solution();
+        sol.lengthOfLongestSubstring("pwwkew");
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        char[] sArray = s.toCharArray();
+        int len = sArray.length;
+
+        if (len == 0) {
+            return 0;
+        }
+
+        int maxLength = 0;
+        Set<Character> unique = new HashSet<>();
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                if (unique.contains(sArray[j])) {
+                    unique.clear();
+                    break;
+                }
+
+                unique.add(sArray[j]);
+                maxLength = Math.max(maxLength, i == 0? j - i + 1: j - i + 1);
+            }
+        }
+
+        return maxLength;
+    }
+
+    /*
     public int lengthOfLongestSubstring(String s) {
         char[] sArray = s.toCharArray();
         int len = sArray.length;
@@ -34,4 +68,6 @@ public class Solution {
 
         return maxLength;
     }
+
+     */
 }
