960bcba12caf40c1e78069476cbfc312f7e88430
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2022-01-26 18:00:28 +0530

    Leetcode problem

--- src/array/ValidMountainArray941/Solution.java
+++ src/array/ValidMountainArray941/Solution.java
@@ -4,12 +4,14 @@ public class Solution {
 
     public static void main(String[] str) {
         Solution sol = new Solution();
-        /*sol.validMountainArray(new int[]{2, 1});
+        sol.validMountainArray(new int[]{2, 1});
         sol.validMountainArray(new int[]{3, 5, 5});
         sol.validMountainArray(new int[]{0, 3, 2, 1});
-
-         */
         sol.validMountainArray(new int[]{0, 1, 2, 1, 2});
+        sol.validMountainArray(new int[]{2, 0, 2});
+        sol.validMountainArray(new int[]{1, 2, 3});
+        sol.validMountainArray(new int[]{3, 2, 1});
+        sol.validMountainArray(new int[]{1, 3, 2});
     }
 
     public boolean validMountainArray(int[] arr) {
@@ -19,25 +21,27 @@ public class Solution {
             return false;
         }
 
-        int curDiff = 0;
+        int index = 1;
 
-        boolean peakReached = false;
+        boolean firstInc = false;
 
-        for (int i = 2; i < len; i++) {
-            curDiff = arr[i] - arr[i -1];
+        while (index < len && arr[index - 1] < arr[index]) {
+            firstInc = true;
+            index++;
+        }
 
-            if (curDiff < 0) {
-                peakReached = true;
-                continue;
-            }
+        if (!firstInc) {
+            return false;
+        }
 
-            if (curDiff == 0) {
-                return false;
-            }
+        boolean dec = false;
+        while (index < len && arr[index - 1] > arr[index]) {
+            dec = true;
+            index++;
+        }
 
-            if (peakReached && curDiff > 0) {
-                return false;
-            }
+        if (!dec || index != len) {
+            return false;
         }
 
         return true;
