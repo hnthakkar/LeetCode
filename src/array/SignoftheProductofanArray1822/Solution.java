75634e5afd665913837c1bde1705fc83e9a9e90c
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2022-03-08 15:13:58 +0530

    Leetcode problem

--- src/array/SignoftheProductofanArray1822/Solution.java
+++ src/array/SignoftheProductofanArray1822/Solution.java
@@ -4,13 +4,13 @@ public class Solution {
 
     public static void main(String[] str) {
         Solution sol = new Solution();
-        sol.arraySign(new int[] {41,65,14,80,20,10,55,58,24,56,28,86,96,10,3,84,4,41,13,32,42,43,83,78,82,70,15,-41});
+        sol.arraySign(new int[]{41, 65, 14, 80, 20, 10, 55, 58, 24, 56, 28, 86, 96, 10, 3, 84, 4, 41, 13, 32, 42, 43, 83, 78, 82, 70, 15, -41});
     }
 
     public int arraySign(int[] nums) {
         int negativeCount = 0;
 
-        for (int n: nums) {
+        for (int n : nums) {
             if (n == 0) {
                 return 0;
             }
