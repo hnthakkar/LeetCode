da853cdd7b08347ca2a0fff614251ea37ef68426
hnthakkar <hemal_thakkar_iiit@yahoo.in>
2022-04-17 14:40:45 +0530

    Practise

--- src/tree/BinarySearchTreetoGreaterSumTree1038/Solution.java
+++ src/tree/BinarySearchTreetoGreaterSumTree1038/Solution.java
@@ -1,17 +1,11 @@
-package tree.ConvertBSTtoGreaterTree538;
+package tree.BinarySearchTreetoGreaterSumTree1038;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))), new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))));
-        Solution sol = new Solution();
-        sol.convertBST(root);
-    }
-
-    public TreeNode convertBST(TreeNode root) {
+    public TreeNode bstToGst(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         inOrderList(root, list);
 
@@ -21,10 +15,10 @@ public class Solution {
             list.set(i, list.get(i) + list.get(i + 1));
         }
 
-
+        /*
         for (int i = 0; i < len; i++) {
             System.out.println("Number : " + list.get(i));
-        }
+        }*/
 
 
         updateValues(root, list, 0);
@@ -38,7 +32,7 @@ public class Solution {
         }
 
         int done = updateValues(root.left, inOrderList, index);
-        System.out.println("updating " + root.val + " with value : " + inOrderList.get(done + 1));
+        // System.out.println("updating " + root.val + " with value : " + inOrderList.get(done + 1));
         root.val = inOrderList.get(done + 1);
         done = updateValues(root.right, inOrderList, done + 2);
 
@@ -54,17 +48,18 @@ public class Solution {
         inOrder.add(root.val);
         inOrderList(root.right, inOrder);
     }
+
 }
 
 class TreeNode {
     int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
